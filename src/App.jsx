d2fd9a08116e296b9bf38687770c9f9157cdc458
arodriguez78 <arodriguez78@alu.ucam.edu>
2024-07-29 16:14:35 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
+import Login from "./components/login/Login";
 
 const App = () => {
-  return (
-    <div className='container'>
-      <List/>
-      <Chat/>
-      <Detail/>
-    </div>
-  )
-}
+  const user = false;
 
-export default App
+  return (
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
+};
+
+export default App;
