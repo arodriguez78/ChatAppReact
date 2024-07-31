f97a6b076a1546d1e1a62fa9dd2680226bfa82de
arodriguez78 <arodriguez78@alu.ucam.edu>
2024-07-31 13:28:57 +0200

    updated

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -76,7 +76,7 @@ const Detail = () => {
           </div>
         </div>
         <button>Block User</button>
-        <button className="logout">Logout</button>
+        <button className="logout">Logout!</button>
       </div>
     </div>
   );
