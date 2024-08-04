33e9a39aae57bdca16a2176fb18b6e4aede4778e
arodriguez78 <arodriguez78@alu.ucam.edu>
2024-08-04 13:22:50 +0200

    commit

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -6,7 +6,7 @@ const Detail = () => {
       <div className="user">
         <img src="./avatar.png" alt="" />
         <h2>Gojo</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
       </div>
       <div className="info">
         <div className="option">
